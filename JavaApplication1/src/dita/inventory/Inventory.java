4e65de56df0c96ac06e71c4bf31b5fccd323d1d0
Michael Bukachi <michaelbukachi@gmail.com>
2015-11-16 18:03:57 +0300

    Removed IDE generated Comments

--- JavaApplication1/src/dita/inventory/Inventory.java
+++ JavaApplication1/src/dita/inventory/Inventory.java
@@ -1,20 +1,21 @@
-
 package dita.inventory;
 
+import dita.inventory.miscellaneous.ItemsCollection;
 import dita.inventory.model.Database;
-import dita.inventory.model.ItemsModel;
+import dita.inventory.model.Item;
 
+public class Inventory
+{
 
-public class Inventory {
-
-   
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Database.startConnection();
-       ItemsModel item = new ItemsModel();
-       System.out.println(item.toString());
-       ItemsModel item2 = new ItemsModel ("football", 20, 15);
-       System.out.println(item2.toString());
-       Database.closeConnection();
+        Item item = new Item();
+        System.out.println(item.toString());
+        Item item2 = new Item("football", 20, 15);
+        System.out.println(item2.toString());
+        Database.closeConnection();
+        ItemsCollection list = new ItemsCollection();
     }
-    
+
 }
